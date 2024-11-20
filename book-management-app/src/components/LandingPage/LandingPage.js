036cb565e3de972db6557145f32693d2f22a9253
Aryan Kumar <aimpendance@gmail.com>
2024-11-20 11:08:08 -0700

    Modified Dashboard

--- book-management-app/src/components/LandingPage/LandingPage.js
+++ book-management-app/src/components/LandingPage/LandingPage.js
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom'
 const LandingPage = () => {
     return (
         
-                <div className='landingPageContainer'>
-                    <h1>Landing Page</h1>
+                <div className='landingPageContent'>
+                    <h1>Manage Your Library with Ease</h1>
                     <NavLink to="/login">Login</NavLink>
                     <NavLink to="/login">Get Started</NavLink>
                 </div>
